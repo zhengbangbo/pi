9b8bfa150f34eb8fb4b309b9abf293edd793b4e2
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-08-01 23:19:08 +0800

    chore: remove dist

--- dist/pi.mjs
+++
@@ -1,9 +0,0 @@
-import {
-  parsePi,
-  run
-} from "./chunk-DI7T7WZG.mjs";
-
-// src/pi.ts
-run(async (agent, args) => {
-  return parsePi(agent, args);
-});
--- dist/psh.mjs
+++
@@ -1,9 +0,0 @@
-import {
-  parsePsh,
-  run
-} from "./chunk-DI7T7WZG.mjs";
-
-// src/psh.ts
-run(async (agent, args) => {
-  return parsePsh(agent, args);
-});
